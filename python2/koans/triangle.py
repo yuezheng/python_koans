201849fca29e368fc10f1e277ac275a941a7ee5a
Matt Stevens <matt@dirtymonkey.co.uk>
2013-03-12 02:50:09 +0000

    Lesson 12 - Thinking About Triangle Project: complete

--- python2/koans/triangle.py
+++ python2/koans/triangle.py
@@ -19,7 +19,13 @@
 #
 def triangle(a, b, c):
     # DELETE 'PASS' AND WRITE THIS CODE
-    pass
+    if a == b == c:
+        return "equilateral"
+
+    if a == b or a == c or b == c:
+        return "isosceles"
+
+    return "scalene"
 
 
 # Error class used in part 2.  No need to change this code.
