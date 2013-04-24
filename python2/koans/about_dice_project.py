6cc44ae9f1a9d07f9c0faff2569525135e64b713
Matt Stevens <matt@dirtymonkey.co.uk>
2013-04-24 01:01:17 +0100

    Lesson 23 - Thinking About Dice Project: complete

--- python2/koans/about_dice_project.py
+++ python2/koans/about_dice_project.py
@@ -15,9 +15,9 @@ class DiceSet(object):
         return self._values
 
     def roll(self, n):
-        # Needs implementing!
-        # Tip: random.randint(min, max) can be used to generate random numbers
-        pass
+        from random import randint
+        self._values = []
+        self._values.extend([randint(1, 6) for i in range(n)])
 
 
 class AboutDiceProject(Koan):
